4963a844fa908d5d1d7e144d4537c32dc58480fc
Somon Gulmamadov <soma.gulov@gmail.com>
2022-09-15 11:13:56 +0200

    Class_funk

---
+++ class_and_funk.py
@@ -0,0 +1,18 @@
+
+class Cat:
+    
+    def __init__(self, name, breed="Pars", age=1, color="White"):
+        print("Hello new (object)cat", name, breed, age, color)
+        self.name = name
+        self.breed = breed
+        self.age = age
+        self.color = color
+        return
+
+    
+
+Mona = Cat("Mona", "Siem", 1, "Broun")
+Tom = Cat("Tom")
+print(Mona.name)
+print(Mona)
+print(Tom)
