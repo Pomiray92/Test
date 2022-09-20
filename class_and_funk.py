2ccb486ae48d798a6787a22bf90eb23b9e049822
Somon Gulmamadov <soma.gulov@gmail.com>
2022-09-20 12:49:55 +0200

    class and Funk

--- class_and_funk.py
+++ class_and_funk.py
@@ -2,7 +2,7 @@
 class Cat:
     
     def __init__(self, name, breed="Pars", age=1, color="White"):
-        print("Hello new (object)cat", name, breed, age, color)
+        print("Hello ", name, breed, age, color)
         self.name = name
         self.breed = breed
         self.age = age
@@ -13,6 +13,7 @@ class Cat:
 
 Mona = Cat("Mona", "Siem", 1, "Broun")
 Tom = Cat("Tom")
-print(Mona.name)
-print(Mona)
-print(Tom)
+# print(Mona.name)
+# print(Mona)
+# print(Tom)
+
